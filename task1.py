381e20dc523217ddb629cb819249dc5aa59aed97
Dhanya2008 <dhanyashrisenthilkumar45@gmail.com>
2026-04-21 14:23:36 +0530

    first commit

---
+++ task1.py
@@ -0,0 +1,20 @@
+name="Dhanya shri"
+age=17
+gender="female"
+height=5.0
+weight=61
+school="Cluny Matric Hr Sec School"
+hobby="Listening to music"
+father_name="Senthilkumar"
+mother_name="Sangeetha"
+mother_tongue="Telugu"
+print("Name: ",name)
+print("Age: ",age)
+print("Gender: ",gender)
+print("Height: ",height,"ft")
+print("Weight: ",weight,"kg")
+print("School: ",school)
+print("Hobby: ",hobby)
+print("Father's Name: ",father_name)
+print("Mother's Name: ",mother_name)
+print("Mother Tongue: ",mother_tongue)
