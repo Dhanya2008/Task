e600c0d34d195498e148dc7f4869a9a162c65c84
Dhanya2008 <dhanyashrisenthilkumar45@gmail.com>
2026-04-27 17:29:51 +0530

    task3

---
+++ task3.py
@@ -0,0 +1,6 @@
+annual_income=1000000
+monthly_income=(annual_income/12)
+tax=0.10
+health_insurance=5000
+hand_on_cash=(monthly_income-tax-health_insurance)
+print("Hand on cash per month: ",hand_on_cash)
