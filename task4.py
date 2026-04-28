1e385612c3a84a778a2fda1e65a3ff7c2512bd3f
Dhanya2008 <dhanyashrisenthilkumar45@gmail.com>
2026-04-28 12:08:48 +0530

    task4

---
+++ task4.py
@@ -0,0 +1,8 @@
+blue=100
+green=100
+white=blue+green
+ball=input("Give the colour of the ball:")
+if ball=="blue" or ball=="green":
+    print("You have won: ",blue)
+else:
+    print("You have won: ",white) 
