f6bb2d179829d2cec480f67c408529912cb12aff
Dhanya2008 <dhanyashrisenthilkumar45@gmail.com>
2026-04-29 17:38:04 +0530

    task5

---
+++ task5.py
@@ -0,0 +1,20 @@
+print("Vande Bharat")
+ac_coach=500
+non_ac_coach=300
+coach=input("Enter your coach: ")
+sleeper=450
+general=250
+seat=input("Enter your preferred seat: ")
+if seat=="sleeper" and coach=="ac coach":
+    print("You ticket has been confirmed")
+    print("The total cost will be: ",sleeper+ac_coach)
+if seat=="sleeper" and coach=="non ac coach":
+    print("You ticket has been confirmed")
+    print("The total cost will be: ",sleeper+non_ac_coach)
+if seat=="general"and coach=="ac coach":
+    print("You ticket has been confirmed")
+    print("The total cost will be: ",general+ac_coach)
+if seat=="general" and coach=="non ac coach":
+    print("You ticket has been confirmed")
+    print("The total cost will be: ",general+non_ac_coach)
+print("Thank you for choosing Vande Bharat")
