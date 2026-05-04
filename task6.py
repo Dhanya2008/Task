618c875f0e06171ab8796022dec1d2f685352a18
Dhanya2008 <dhanyashrisenthilkumar45@gmail.com>
2026-05-04 15:58:37 +0530

    task6

--- task6.py
+++ task6.py
@@ -1,4 +1,13 @@
-n=int(input("Enter the no of terms for series:"))
-sequence=[0,1,1]
-total_sum=0
-print("S")
+n=int(input("Enter the number of terms for series"))
+sequence = [0, 1, 1]
+total_sum = 0
+print("Sequence:")
+for i in range(n):
+    if i < 3:
+        current_term = sequence[i]
+    else:
+        current_term = sequence[-1] * 2
+        sequence.append(current_term)
+    print(current_term, end=" ")
+    total_sum += current_term
+print(f"\nSum: {total_sum}")
